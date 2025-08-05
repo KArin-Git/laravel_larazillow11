45995030171e17d5163f92470f6fae81fb6360cb
Arin <arin.apichitchon@gmail.com>
2025-08-04 20:59:46 -0400

    passing data to pages

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,11 +1,15 @@
 <template>
-    Hello! {{ counter }}
+    Hello!
     <Link href="/hello">Show Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
-const counter = ref(0)
-setInterval(() => counter.value++, 1000)
+
+defineProps({
+    message: String
+})
 </script>
