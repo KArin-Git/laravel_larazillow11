8a4c8f5d94f829b028eab41fc0310fc4f968b186
Arin <arin.apichitchon@gmail.com>
2025-08-04 21:07:38 -0400

    Layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,14 @@
 <template>
-    Hello!
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is {{ message }}
-    </div>
+    <MainLayout>
+        <div>Hello!</div>
+        <div>
+            The message is {{ message }}
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import MainLayout from '../../Layouts/MainLayout.vue';
 
 defineProps({
     message: String
