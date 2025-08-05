8935ea754c4a1485333f7022939eecc81133c2d7
Arin <arin.apichitchon@gmail.com>
2025-08-04 20:50:44 -0400

    create first page

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,10 @@
 <template>
     Hello! {{ counter }}
+    <Link href="/hello">Show Page</Link>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
 import { ref } from 'vue'
 const counter = ref(0)
 setInterval(() => counter.value++, 1000)
